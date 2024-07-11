888dd1c82709ade6a8d32a035e1dcd91c1490ba2
Charikshith <chakri7822@gmail.com>
2024-07-11 21:04:37 +0530

    test.py done

--- test.py
+++ test.py
@@ -1,1 +1,54 @@
-print("Hello World")
+import warnings
+import argparse
+import pandas as pd
+import numpy as np
+import logging
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+#get the arguments from the command shell
+parser = argparse.ArgumentParser()
+parser.add_argument("--alpha",type=float,required=False,default=0.2)
+parser.add_argument("--l1_ratio",type=float,required=False,default=0.5)
+args = parser.parse_args()
+
+#evaluation
+def eval_metrics(actual,pred):
+    rmse = np.sqrt(mean_squared_error(actual,pred)) #rootmeansquareerror
+    mae =mean_absolute_error(actual,pred) 
+    r2 = r2_score(actual,pred)
+    return rmse,mae,r2
+
+if __name__=='__main__':
+    warnings.filterwarnings("ignore")
+    np.random.seed(3)
+
+    #Read the csv file from the path mentioned
+    data = pd.read_csv("data/red-wine-quality.csv")
+    
+    # Spliting to train and test
+    train, test = train_test_split(data)
+
+    # Assign X and Y
+    train_x = train.drop(['quality'],axis=1)
+    test_x = test.drop(['quality'],axis=1)
+    train_y = train[['quality']]
+    test_y = test[['quality']]
+
+    alpha = args.alpha
+    l1_ratio = args.l1_ratio
+
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state= 3)
+    lr.fit(train_x,train_y)
+
+    pred1 = lr.predict(test_x)
+
+    (rmse,mea,r2)= eval_metrics(test_y,pred1)
+    print(" ElasticNet Model (alpha={:f}, l1_ratio={:f})".format(alpha,l1_ratio))
+    print("RMSE:{rmse} \n MAE:{mae} \n R2:{r2}")
+    
